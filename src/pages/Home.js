708247154c9deb8686a33325c924b0f6d95ef4e6
ada-rsh-s <aadarsh04ss@gmail.com>
2024-03-21 22:53:51 +0530

    Project structure changed

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import "../css/Home.css";
+import logo from "../img/header_jy.png";
+import liveey from "../img/livv.png";
+function Home() {
+  return (
+    <div className="App">
+
+      <div className="header">
+        <p>Laya</p>
+        <img src={logo} alt="jylogo" className="logog"></img>
+      </div>
+      <div className="livee">
+        <img src={liveey} alt="bggs"></img>
+        <div className="lvp">
+          <iframe src="https://lottie.host/embed/da624969-2143-4bb0-9084-fe63affd0c5b/1F4mejgP7z.json"></iframe>
+          <p className="ln">Live Now</p>
+          <p className="item1">Kathakali</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
