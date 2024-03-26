a2a9f0c408e20ee84a45a617d3737c42f0bfcb26
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-03-26 14:51:44 +0530

    Schedule

--- src/pages/Files.js
+++ src/pages/Files.js
@@ -62,54 +62,76 @@ function Files() {
         </div>
       </div>
         
-      {/* <h2>On Stage Items</h2>
+       <h2>On Stage Items</h2>
       <div className="scheduler">
-        <h3>Stage 1 - NoName</h3>
-        <p className='sloc'>Auditorium  - <b>26/03/24</b></p>
+        <h3>Stage 1 - Greeshmam</h3>
+        <p className='sloc'>Auditorium  - <b>27/03/24</b></p>
         <div className="sambhavamso">
-        <p><b>Essay Writing English </b>- 10.00 to 11.00 -</p>
-        <p><b>Essay Writing Malyalam</b> - 11.00 to 12.00</p>
-        <p><b>Essay Writing Hindi</b> -12.00 to 01.00</p>
-        <p><b>Essay Writing Sanksrit</b> - 2.00 to 3.00</p>
-        <p><b>Story Writing English</b> -1.00 to 2.00</p>
+        <p><b>Mime </b>- 10.20 to 10.35 -</p>
+        <p><b>Thiruvathirakali Girls</b> - 10.40 to 11.20</p>
+        <p><b>Fancy Dress</b> -11.20 to 11.40</p>
+        <p><b>Daffmutt Boys</b> - 11.40 to 11.50</p>
+        <p><b>Kolkali Boys</b> -11.50 to 12.00</p>
+        <p><b>Vattapatt Boys</b> -12.00 to 12.15</p>
+        <p><b>Bharathanatyam Girls</b> -12.15 to 12.30</p>
+        <p><b>Group Dance Boys</b> -01.00 to 02.00</p>
+        <p><b>Group Dance Girls</b> -02.00 to 03.30</p>
         </div>
       </div>
 
       <div className="scheduler">
-        <h3>Stage 2 - Bhavanika</h3>
-        <p className='sloc'>EAB 415  - <b>25/03/24</b></p>
+        <h3>Stage 2 - Vasantham</h3>
+        <p className='sloc'>EEE Seminar Hall  - <b>27/03/24</b></p>
         <div className="sambhavamso">
-        <p><b>Poster Making </b>- 10.30 to 11.30</p>
-        <p><b>Collage</b> - 11.30 to 12.30</p>
-        </div>
-        <p className='sloc'>EAB 415  - <b>26/03/24</b></p>
+        <p><b>Elocution Malayalam </b>- 10.00 to 10.50</p>
+        <p><b>Elocution Hindi</b> - 10.50 to 11.30</p>
+        <p><b>Mimicry</b> - 11.30 to 11.45</p>
+        <p><b>Monoact</b> - 11.45 to 12.30</p>
+      </div>
+
+      <div className="scheduler">
+        <h3>Stage 3 - Shishiram</h3>
+        <p className='sloc'>ME Seminar Hall - <b>27/03/24</b></p>
         <div className="sambhavamso">
-        <p><b>Versification English </b>- 11.30 to 12.30 -</p>
-        <p><b>Versification Malyalam</b> - 2.30 to 3.30</p>
-        <p><b>Versification Sanksrit</b> - 1.30 to 2.30</p>
+        <p><b>Western Song </b>- 10.00 to 11.10</p>
+        <p><b>String</b> - 11.10 to 11.30</p>
+        <p><b>Wind</b> - 11.30 to 11.50</p>
+        <p><b>Percussion</b> - 11.50 to 12.15</p>
+        <p><b>Kathaprasangam</b> - 12.15 to 12.30</p>
         </div>
       </div>
 
       <div className="scheduler">
-        <h3>Stage 3 - Dhwani</h3>
-        <p className='sloc'>OPEN LIBRARY  - <b>26/03/24</b></p>
+        <h3>Stage 4 - Varsham</h3>
+        <p className='sloc'>EAB 203 - <b>27/03/24</b></p>
         <div className="sambhavamso">
-        <p><b>Debate English </b>- 09.30 to 10.30</p>
-        <p><b>Debate Malayalam</b> - 10.30 to 11.30</p>
+        <p><b>Elocution English </b>- 10.00 to 11.15</p>
+        <p><b>Kavithaparayanam</b> - 11.15 to 12.30</p>
         </div>
       </div>
 
       <div className="scheduler">
-        <h3>Stage 4 - Mazhavil</h3>
-        <p className='sloc'>ME Drawing Hall  - <b>25/03/24</b></p>
+        <h3>Stage 5 - Hemantham</h3>
+        <p className='sloc'>EAB 104 - <b>27/03/24</b></p>
         <div className="sambhavamso">
-        <p><b>Cartoon </b>- 09.30 to 10.30</p>
-        <p><b>Oil Paint</b> - 09.30 to 10.30</p>
-        <p><b>Pencil Drawing</b> - 11.30 to 12.30</p>
-        <p><b>Water Colouring</b> - 01.30 to 02.30</p>
+        <p><b>Lalithagaanam Boys </b>- 10.00 to 10.40</p>
+        <p><b>Lalithagaanam Girls</b> - 10.40 to 11.50</p>
+        <p><b>Classical Song Girls</b> - 11.50 to 12.05</p>
+        <p><b>Classical Song Boys</b> - 12.05 to 12.30</p>
         </div>
-      </div> */}
-  
+      </div>
+
+      <div className="scheduler">
+        <h3>Stage 6 - Dhwani</h3>
+        <p className='sloc'>Open Library - <b>27/03/24</b></p>
+        <div className="sambhavamso">
+        <p><b>Group Song Western</b>- 10.00 to 10.20</p>
+        <p><b>Group Song Eastern</b> - 10.20 to 10.40</p>
+        <p><b>Naadanpattu</b> - 10.45 to 12.15</p>
+        </div>
+      </div>
+
+  </div>
     </div>
   )
 }
