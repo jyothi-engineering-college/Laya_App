af585801e2fb5416389593cc8d139b7b79c6426c
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-03-26 10:04:36 +0530

    onedignsl2

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,14 +6,13 @@ import {getFirestore} from "@firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgLJXSyESeyfTGksdUiLqSVgZ6L5lTcGw",
-    authDomain: "laya-5f753.firebaseapp.com",
-    projectId: "laya-5f753",
-    storageBucket: "laya-5f753.appspot.com",
-    messagingSenderId: "768132343455",
-    appId: "1:768132343455:web:c60d2b7b33e3d5749ee4aa",
-    measurementId: "G-Q99JFWHW3Y"
-  };
+  apiKey: "AIzaSyDDmeU9nNlFzGRk0HziNNpWYwhl3kEzi1E",
+  authDomain: "laya-app-4ef7c.firebaseapp.com",
+  projectId: "laya-app-4ef7c",
+  storageBucket: "laya-app-4ef7c.appspot.com",
+  messagingSenderId: "409888583397",
+  appId: "1:409888583397:web:dbfc82eff04f0138636e0c"
+};
 
   const app = initializeApp(firebaseConfig);
 
