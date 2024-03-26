5a1cebc8095b7eb7338cb44928ec11af48392689
ada-rsh-s <aadarsh04ss@gmail.com>
2024-03-26 20:06:41 +0530

    stlyling changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,41 +3,53 @@ import logo from "../img/header_jy.png";
 
 import { IonContent } from "@ionic/react";
 import Header from "../components/header/Header";
-import LatestResults from "../components/LatestResults/LatestResults";
+import LatestResults from "../components/LatestResults/LatestoffResults";
 import UpcomingEvents from "../components/UpcomingEvents/UpcomingEvents";
 import TopHouses from "../components/TopHouses/TopHouses";
+import LatestonResults from "../components/LatestResults/LatestonResults";
+import LatestoffResults from "../components/LatestResults/LatestoffResults";
 
 function Home() {
   return (
-
-  
-
-    <IonContent>     
+    <IonContent>
       <div className="App">
         <div className="header">
-          <img style={{borderRadius:"50%",height:"50px",marginRight:"-150px"}} srcSet={require("../img/logo.jpg")} alt="" src="" />
+          <img
+            style={{
+              borderRadius: "50%",
+              height: "50px",
+              marginRight: "-150px",
+            }}
+            srcSet={require("../img/logo.jpg")}
+            alt=""
+            src=""
+          />
 
           <p>Laya</p>
           <img srcSet={logo} alt="jylogo" className="logog"></img>
         </div>
 
         <Header docId="header"></Header>
-
-        <p className="rhd">Latest Results</p>
+{/* 
+        <p className="rhd">Latest ON-STAGE&nbsp; Results</p>
         <div className="shoo"></div>
 
-        <LatestResults docId="latestres"></LatestResults>
+        <LatestonResults docId="latestres"></LatestonResults> */}
+        <p className="rhd">Latest OFF-STAGE&nbsp; Results</p>
+        <div className="shoo"></div>
+
+        <LatestoffResults docId="latestres"></LatestoffResults>
 
         <p className="rhd">Upcoming Events</p>
         <div className="shoo"></div>
 
         <UpcomingEvents></UpcomingEvents>
 
-      <p className="rhd">Top Houses</p>
-      <div className="shoo"></div>
-      
-      <TopHouses docId="tophouse"></TopHouses>
-      <div className="sthalam"></div>
+        <p className="rhd">Top Houses</p>
+        <div className="shoo"></div>
+
+        <TopHouses docId="tophouse"></TopHouses>
+        <div className="sthalam"></div>
       </div>
     </IonContent>
   );
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -20,49 +20,52 @@ const Result = () => {
         <h1 className="resultd">Result to be declared</h1>
       ) : (
         <table>
-          <tr>
-            <th>Name</th>
-            <th>Class</th>
-            <th>House</th>
-            <th>Position</th>
-          </tr>
-          <td colspan="4">
-            <hr className="headerhr" width="90%" />
-          </td>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Class</th>
+              <th>House</th>
+              <th>Position</th>
+            </tr>
 
-          <td colspan="4">
-            <hr className="rowshr" width="90%" />
-          </td>
-          <tr>
-            <td>{eventResult["name-1"]}</td>
-            <td>{eventResult["class-1"]}</td>
-            <td>{eventResult["house-1"]}</td>
-            <td className="place1">First</td>
-          </tr>
-          <td colspan="4">
-            <hr className="rowshr" width="90%" />
-          </td>
-          <tr>
-            <td>{eventResult["name-2"]}</td>
-            <td>{eventResult["class-2"]}</td>
-            <td>{eventResult["house-2"]}</td>
-            <td className="place1">Second</td>
-          </tr>
-          <td colspan="4">
-            <hr className="rowshr" width="90%" />
-          </td>
-          <tr>
-            <td>{eventResult["name-3"]}</td>
-            <td>{eventResult["class-3"]}</td>
-            <td>{eventResult["house-3"]}</td>
-            <td className="place1">Third</td>
-          </tr>
-          <td colspan="4">
-            <hr className="rowshr" width="90%" />
-          </td>
+            <td colSpan="4">
+              <hr className="headerhr" width="90%" />
+            </td>
+
+            <td colSpan="4">
+              <hr className="rowshr" width="90%" />
+            </td>
+            <tr>
+              <td>{eventResult["name-1"]}</td>
+              <td>{eventResult["class-1"]}</td>
+              <td>{eventResult["house-1"]}</td>
+              <td className="place1">First</td>
+            </tr>
+            <td colSpan="4">
+              <hr className="rowshr" width="90%" />
+            </td>
+            <tr>
+              <td>{eventResult["name-2"]}</td>
+              <td>{eventResult["class-2"]}</td>
+              <td>{eventResult["house-2"]}</td>
+              <td className="place1">Second</td>
+            </tr>
+            <td colSpan="4">
+              <hr className="rowshr" width="90%" />
+            </td>
+            <tr>
+              <td>{eventResult["name-3"]}</td>
+              <td>{eventResult["class-3"]}</td>
+              <td>{eventResult["house-3"]}</td>
+              <td className="place1">Third</td>
+            </tr>
+            <td colSpan="4">
+              <hr className="rowshr" width="90%" />
+            </td>
+          </tbody>
+         
         </table>
       )}
-      <center></center>
       {/* </div> */}
     </div>
   );
