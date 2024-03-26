109478dc8a44ba6c403fbe81ef6e20c24d260027
ada-rsh-s <aadarsh04ss@gmail.com>
2024-03-26 10:45:59 +0530

    postion

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -16,7 +16,10 @@ const Result = () => {
         <div className="imgevent"></div>
       </center>
       <div className="eventname">{resultName}</div>
-      {(eventResult["name-1"]=="dummy")?<h1 className="resultd">Result to be declared</h1>: <table>
+      {eventResult["name-1"] == "dummy" ? (
+        <h1 className="resultd">Result to be declared</h1>
+      ) : (
+        <table>
           <tr>
             <th>Name</th>
             <th>Class</th>
@@ -34,7 +37,7 @@ const Result = () => {
             <td>{eventResult["name-1"]}</td>
             <td>{eventResult["class-1"]}</td>
             <td>{eventResult["house-1"]}</td>
-            <td className="place1">{eventResult["position-1"]}</td>
+            <td className="place1">First</td>
           </tr>
           <td colspan="4">
             <hr className="rowshr" width="90%" />
@@ -43,7 +46,7 @@ const Result = () => {
             <td>{eventResult["name-2"]}</td>
             <td>{eventResult["class-2"]}</td>
             <td>{eventResult["house-2"]}</td>
-            <td className="place1">{eventResult["position-2"]}</td>
+            <td className="place1">Second</td>
           </tr>
           <td colspan="4">
             <hr className="rowshr" width="90%" />
@@ -52,14 +55,14 @@ const Result = () => {
             <td>{eventResult["name-3"]}</td>
             <td>{eventResult["class-3"]}</td>
             <td>{eventResult["house-3"]}</td>
-            <td className="place1">{eventResult["position-3"]}</td>
+            <td className="place1">Third</td>
           </tr>
           <td colspan="4">
             <hr className="rowshr" width="90%" />
           </td>
-        </table>}<center>
-       
-      </center>
+        </table>
+      )}
+      <center></center>
       {/* </div> */}
     </div>
   );
