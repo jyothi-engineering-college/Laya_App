bf1ca22c60c7912674dd8f75944ef0e3c9b767fb
ada-rsh-s <aadarsh04ss@gmail.com>
2024-03-28 14:45:39 +0530

    laya 4

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,14 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8q8lrTGwxg9CrEjaE7T7P_q9rlNolLcE",
-  authDomain: "laya-5.firebaseapp.com",
-  projectId: "laya-5",
-  storageBucket: "laya-5.appspot.com",
-  messagingSenderId: "286108118417",
-  appId: "1:286108118417:web:1845f539c53a6002497e30",
-  measurementId: "G-0HXL9DRCD1",
+  apiKey: "AIzaSyDhyojtfpE8ibzn-i-PVedgkPMo28SSmVw",
+  authDomain: "laya-4.firebaseapp.com",
+  projectId: "laya-4",
+  storageBucket: "laya-4.appspot.com",
+  messagingSenderId: "933689873372",
+  appId: "1:933689873372:web:e68b08f229f54690be3519",
+  measurementId: "G-029WQEG6XZ",
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
